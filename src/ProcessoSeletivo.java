6faf6c116df42bf2fe5e952657f440605ef032c6
vitorbizarra <vitorbizarracontato@gmail.com>
2023-08-21 22:21:21 -0300

    :sparkles: feat: Método `imprimirSelecionados()`

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -2,7 +2,22 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados() {
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO" };
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de nº " + (indice + 1) + " é " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+
+        for(String candidato: candidatos){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos() {
